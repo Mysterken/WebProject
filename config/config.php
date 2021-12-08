a89fe2dd00f94eb88af759b58c813c9cd3f101fc
Mysterken <Mysterkenzo@gmail.com>
2021-12-08 11:08:45 +0100

    First upload

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+//site name
+define('SITE_NAME', 'WebProject');
+
+//App Root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+define('URL_ROOT', '/');
+define('URL_SUBFOLDER', '');
+
+//DB Params
+define('DB_HOST', 'your-host');
+define('DB_USER', 'root');
+define('DB_PASS', 'example');
+define('DB_NAME', 'your-db-name');
